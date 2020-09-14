8b905c084f0edbddac5d75c342ffdb2e2e54f769
Mahavir Munot <mahavir1408@gmail.com>
2020-09-14 14:29:37 +0530

    Updated counter example

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -1,42 +1,34 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["Tag1", "Tag2", "Tag3"],
-  };
-
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
-
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           className="button btn-secondary btn-sm"
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
