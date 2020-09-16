8e42d6ee6f2c46bfd0a32bc227bf4f889a064117
Mahavir Munot <mahavir1408@gmail.com>
2020-09-17 00:26:19 +0530

    Updated the apps

--- counter-app/src/components/counter.js
+++ counter-app/src/components/counter.js
@@ -8,20 +8,31 @@ class Counter extends Component {
 
   render() {
     return (
-      <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          className="button btn-secondary btn-sm"
-          onClick={() => this.props.onIncrement(this.props.counter)}
-        >
-          Increment
-        </button>
-        <button
-          className="btn btn-danger btn-sm m-2"
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-        >
-          Delete
-        </button>
+      <div className="row">
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            className="button btn-secondary btn-sm"
+            onClick={() => this.props.onIncrement(this.props.counter)}
+          >
+            +
+          </button>
+          <button
+            className="button btn-secondary btn-sm m-2"
+            onClick={() => this.props.onDecrement(this.props.counter)}
+            disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+          >
+            X
+          </button>
+        </div>
       </div>
     );
   }
---
+++ vidly/src/components/common/like.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+const Like = ({ liked, onClick }) => {
+  let classes = "fa fa-heart";
+  if (!liked) classes += "-o";
+  return (
+    <i
+      className={classes}
+      onClick={onClick}
+      aria-hidden="true"
+      style={{ cursor: "pointer" }}
+    ></i>
+  );
+};
+
+export default Like;
